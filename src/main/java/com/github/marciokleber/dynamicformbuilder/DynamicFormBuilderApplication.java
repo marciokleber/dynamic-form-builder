1b2bc494086e6ae77f30aaa3bd8e475ee482cd86
Marcio Kleber Junior Lopes de Souza <marciokjls@gmail.com>
2025-11-10 17:14:29 -0300

    Initializing project

---
+++ src/main/java/com/github/marciokleber/dynamicformbuilder/DynamicFormBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.github.marciokleber.dynamicformbuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicFormBuilderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynamicFormBuilderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/marciokleber/dynamicformbuilder/DynamicFormBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.marciokleber.dynamicformbuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicFormBuilderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
